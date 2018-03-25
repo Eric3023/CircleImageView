a194602b47222e7dbffe4aa80585842e107c14d4
Eric3023 <827142667@qq.com>
2018-03-25 10:43:15 +0800

    init

--- app/src/main/java/com/dong/circleimageviewdemo/activity/RoundImageViewActivity.java
+++ app/src/main/java/com/dong/circleimageviewdemo/activity/RoundImageViewActivity.java
@@ -18,7 +18,7 @@ public class RoundImageViewActivity extends AppCompatActivity {
         RoundImageView roundImageView =findViewById(R.id.roundimageview);
         roundImageView
                 .setResourceID(R.drawable.img0)//圆角图片源为资源图片时
-//                .setPath(UrlConfige.IMG_URL)//圆角图片源为网咯图片
+                .setPath(UrlConfige.IMG_URL)//圆角图片源为网咯图片
                 .setRadius((int) getResources().getDimension(R.dimen.x10))//圆角弧度
                 .setImageScale(RoundImageView.CENTER_CROP)//填充类型
                 .setBackgroudColor(Color.WHITE);//圆角图片的背景颜色
